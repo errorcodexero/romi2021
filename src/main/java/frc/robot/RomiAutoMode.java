91537c40f226ea56a84bba0da2b009157ceacf42
Jack (Butch) Griffin <butchg@comcast.net>
2021-05-11 12:43:12 -0700

    added a simple automode as the default

--- src/main/java/frc/robot/RomiAutoMode.java
+++ src/main/java/frc/robot/RomiAutoMode.java
@@ -5,24 +5,25 @@ import org.xero1425.base.actions.DelayAction;
 import org.xero1425.base.actions.InvalidActionRequest;
 import org.xero1425.base.controllers.AutoController;
 import org.xero1425.base.controllers.AutoMode;
-import org.xero1425.base.tankdrive.TankDriveFollowPathAction;
+import org.xero1425.base.tankdrive.TankDrivePowerAction;
 import org.xero1425.base.tankdrive.TankDriveSubsystem;
-import org.xero1425.misc.BadParameterTypeException;
-import org.xero1425.misc.MissingParameterException;
-import org.xero1425.misc.MissingPathException;
 
 public class RomiAutoMode extends AutoMode {
-    public RomiAutoMode(AutoController ctrl)
-            throws InvalidActionRequest, MissingPathException, BadParameterTypeException, MissingParameterException {
+    public RomiAutoMode(AutoController ctrl) throws InvalidActionRequest {
         super(ctrl, "RomiAutoMode") ;
         Action act ;
-
         TankDriveSubsystem db = ctrl.getRobot().getRobotSubsystem().getDB() ;
 
-        // act = new DelayAction(ctrl.getRobot(), 6.0) ;
-        // addAction(act);
+        // Drive for 2 seconds
+        act = new TankDrivePowerAction(db, 0.5, 0.5, 2.0) ;
+        addSubActionPair(db, act, true);
 
-        act = new TankDriveFollowPathAction(db, "TestRomi_Straight", false) ;
+        // Delay for 1 second
+        act = new DelayAction(ctrl.getRobot(), 1.0) ;
+        addAction(act);
+
+        // Drive for 2 seconds
+        act = new TankDrivePowerAction(db, 0.5, 0.5, 2.0) ;       
         addSubActionPair(db, act, true);
     }
 }
